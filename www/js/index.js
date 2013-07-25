71ce0fe6a235c82e9d412e211fc98ecb5c84ae51
Stuart Byrne <strtbyrne@gmail.com>
2013-07-25 10:03:25 +0100

    commit 4
    dynamic titles from json file

--- www/js/index.js
+++ www/js/index.js
@@ -1,62 +1,63 @@
 $(function(){		
 		
-	$.extend({
-		getTitles: function(url) {
-			var result = null;
-			$.ajax({
-				url: url,
-				type: 'get',
-				dataType: 'json',
-				async: false,
-				success: function(data) {
-					result = data;
-				}
-			});
-		   return result;
-		}
-	});
-	
-	var titles = $.getTitles("titles.json"),
-		screenList = [],
-		moduleTitle = titles.course.moduleTitle,
-		moduleCode = titles.course.moduleCode,
-		menuList = $('#list');
-		$screen = $(titles.course.section.screen);
-		
-	$screen.each(function(i){
-				var num = i + 1;
-				var title = $(this).attr('title');
-				screenList.push(title);
-				$('#0' + num).html('Screen ' + num + ': ' + $(this).attr('title'));
-				menuList.append(
-					$('<li />', {
-						text: 'Screen ' + num + ': ' + title,
-						'class': 'screenList',
-						id: '0' + num
-					})
-				);
-		});	
-	
-	var	numScreen = $screen.length,
-		screen = 1,
-		playing = 1,
-		movie = $('#myMovie'),
-		lessonTitle=$('.lessonTitle'),
-		screenTitle = $('.screenTitle'),
-		lessonCode = $('.lessonCode'),
-		mute = 1,
-		$slider = $('#slider'),
-		$slider_value = $('#slider_value'),
-		$nextButton = $('#nextButton'),
-		$prevButton = $('#prevButton'),
-		$nav = $('#nav');
-		//buttonText=document.getElementById('playButton');
-	
-	screenTitle.html(screenList[screen - 1]);
-	lessonTitle.html(moduleTitle);
-	lessonCode.html(moduleCode);	      
-		
+function jsonTitles(holdData){
 
+    $.ajax({
+        type: 'GET',
+        url: 'http://www.skillpad.com/uploads/json/titles.json?callback=?',
+        async: false,
+        jsonpCallback: 'jsonCallback',
+        contentType: "application/json",
+        dataType: 'jsonp',
+        success: function(data) {
+           holdData(data);
+        },
+        error: function(e) {
+           console.log(e.message);
+        }
+    });
+}
+    
+jsonTitles(function(titles){
+   /* alert(titles.course.moduleTitle);*/
+
+        var	screenList = [],
+            moduleTitle = titles.course.moduleTitle,
+            moduleCode = titles.course.moduleCode,
+            menuList = $('#list'),
+            $screen = $(titles.course.section.screen);
+            
+        $screen.each(function(i){
+                    var num = i + 1,
+                        id = $(this).attr('id'),
+                        title = $(this).attr('title'),
+                        desc = $(this).attr('description');
+            
+                    screenList.push(title);
+                    $('#0' + num).html('Screen ' + num + ': ' + $(this).attr('title'));
+                    
+                    menuList.append(
+                        $('<li />', {
+                            'class': 'lesson',
+                            id: 'Screen_0' + num
+                        }).html('<a href="#screenView' + num + '"><img src="images/screen' + num + '.png"><span>' + title + '</span><p>' + desc +'</p></a>'));
+            });
+        
+       /* var	numScreen = $screen.length,
+            screen = 1,
+            playing = 1,
+            movie = $('#myMovie'),
+            lessonTitle=$('.lessonTitle'),
+            screenTitle = $('.screenTitle'),
+            lessonCode = $('.lessonCode'),
+            $nav = $('#nav');
+            //buttonText=document.getElementById('playButton');
+        
+        screenTitle.html(screenList[screen - 1]);
+        lessonTitle.html(moduleTitle);
+        lessonCode.html(moduleCode);	      
+            */
+    });
 
 });
 
